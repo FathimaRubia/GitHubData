94efb27d43caa8f4a71e66aadf12cb9d95b7b2df
FathimaRubia <dazzler_rubia@yahoo.co.in>
2020-10-24 13:52:18 +0530

    For loop program attached

--- src/test/java/com/git/AddingClass.java
+++ src/test/java/com/git/AddingClass.java
@@ -3,7 +3,12 @@ package com.git;
 public class AddingClass {
 
 	public static void main(String[] args) {
-		
-	}
+		for (int i = 1; i <= 100; i++) {
+			if (i == 5) {
+				System.out.println(i);
 
+			}
+		}
+
+	}
 }
