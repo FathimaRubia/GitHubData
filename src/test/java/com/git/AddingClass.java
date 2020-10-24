1a6a1709e298cd6f300550caeebf85a27fe4d8f4
FathimaRubia <dazzler_rubia@yahoo.co.in>
2020-10-24 13:25:45 +0530

    scanner program

--- src/test/java/com/git/AddingClass.java
+++ src/test/java/com/git/AddingClass.java
@@ -1,9 +1,28 @@
 package com.git;
 
+import java.util.Scanner;
+
 public class AddingClass {
 
 	public static void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Please enter your pin number");
+		int pinNo=s.nextInt();
+		System.out.println("Please enter your name");
+		String name=s.next();
+		System.out.println("Enter your salary");
+		float sal=s.nextFloat();
+		System.out.println("----------------------");
+		System.out.println(pinNo);
+		System.out.println(name);
+		System.out.println(sal);
+
+
+
+
+
+	
+
 	}
 
 }
